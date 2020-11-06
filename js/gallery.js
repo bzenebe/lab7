38af8f8163c829e7d95c535550cae533ae9bf9c6
Bee <36945689+bzenebe@users.noreply.github.com>
2020-11-05 22:54:51 -0600

    Add files via upload

---
+++ js/gallery.js
@@ -0,0 +1,21 @@
+
+// create variable equal to all gallery images in document.
+// select all elements with class .imgContainer
+let galleryImages = document.querySelectorAll(".imgContainer");
+// this one keeps track of opened images, should not equal anything.
+let getLastOpenedImage;
+// grab current width of window
+let windowWidth = window.innerWidth;
+
+
+// run condition to see if images exist here.
+if(galleryImages){
+}
+
+
+function closeImg(){
+}
+
+function changeImage(changed){
+
+}
